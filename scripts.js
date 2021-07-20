b0503e3fcf4f898ea4804128207a3f2c820f91ff
Adam Lord <adam@tivix.com>
2021-07-20 15:21:06 -0700

    cleanup

--- scripts.js
+++ scripts.js
@@ -5,12 +5,8 @@ window.dataTable = () => ({
   loading: true,
   searchInput: "",
   total: null,
-  perPage: 10,
+  perPage: 0,
   currentPage: 1,
-  sorted: {
-    field: "name",
-    desc: false,
-  },
   initData() {
     this.fetchData();
   },
@@ -29,56 +25,4 @@ window.dataTable = () => ({
     this.currentPage = n;
     this.fetchData();
   },
-  // compareOnKey(key, rule) {
-  //   return (a, b) => {
-  //     if (
-  //       key === "name" ||
-  //       key === "job" ||
-  //       key === "email" ||
-  //       key === "country"
-  //     ) {
-  //       let comparison = 0;
-  //       const fieldA = a[key].toUpperCase();
-  //       const fieldB = b[key].toUpperCase();
-  //       if (rule === "asc") {
-  //         if (fieldA > fieldB) {
-  //           comparison = 1;
-  //         } else if (fieldA < fieldB) {
-  //           comparison = -1;
-  //         }
-  //       } else if (fieldA < fieldB) {
-  //         comparison = 1;
-  //       } else if (fieldA > fieldB) {
-  //         comparison = -1;
-  //       }
-  //       return comparison;
-  //     }
-  //     if (rule === "asc") {
-  //       return a.year - b.year;
-  //     }
-  //     return b.year - a.year;
-  //   };
-  // },
-  // search(value) {
-  //   if (value.length > 1) {
-  //     const options = {
-  //       shouldSort: true,
-  //       keys: ["name", "job"],
-  //       threshold: 0,
-  //     };
-  //     const fuse = new Fuse(data, options);
-  //     this.items = fuse.search(value).map((elem) => elem.item);
-  //   } else {
-  //     this.items = data;
-  //   }
-  //   // console.log(this.items.length)
-
-  //   this.changePage(1);
-  //   this.showPages();
-  // },
-  // sort(field, rule) {
-  //   this.items = this.items.sort(this.compareOnKey(field, rule));
-  //   this.sorted.field = field;
-  //   this.sorted.rule = rule;
-  // },
 });
