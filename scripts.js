6ffba532af72e11ff8867895e6250c84a53ea139
Adam Lord <adam@tivix.com>
2021-07-21 12:02:35 -0700

    fancier pagination and add searching

--- scripts.js
+++ scripts.js
@@ -1,28 +1,63 @@
 const BASE_URL = "https://swapi.dev/api/planets";
 
 window.dataTable = () => ({
+  // properties
   rows: [],
   loading: true,
   searchInput: "",
-  total: null,
-  perPage: 0,
+  prevSearch: "",
+  totalRecords: null,
+  pageSize: 10,
+  spreadSize: 3,
   currentPage: 1,
+  // methods
   initData() {
     this.fetchData();
   },
   fetchData() {
     this.loading = true;
-    fetch(`${BASE_URL}?page=${this.currentPage}`)
+    if (this.prevSearch !== this.searchInput) {
+      this.currentPage = 1;
+    }
+    fetch(`${BASE_URL}?page=${this.currentPage}&search=${this.searchInput}`)
       .then((response) => response.json())
       .then((data) => {
         this.rows = data.results;
-        this.total = data.count;
-        this.perPage = data.results.length;
+        this.totalRecords = data.count;
+        this.pageSize = data.results.length;
         this.loading = false;
+        this.prevSearch = this.searchInput;
       });
   },
   goToPage(n) {
     this.currentPage = n;
     this.fetchData();
+  },
+  search() {
+    this.fetchData();
+  },
+  // filters
+  intComma(val) {
+    if (Number.isNaN(parseInt(val, 10))) {
+      return val;
+    }
+    return parseInt(val, 10).toLocaleString();
+  },
+  // getters
+  pageSpread() {
+    const range = [];
+    const start = Math.max(this.currentPage - (this.spreadSize - 1), 1);
+    const end = Math.min(
+      this.currentPage + (this.spreadSize - 1),
+      Math.ceil(this.totalRecords / this.pageSize)
+    );
+    for (let i = start; i <= end; i += 1) {
+      range.push(i);
+    }
+    return range;
+  },
+  // computed
+  get totalPages() {
+    return Math.ceil(this.totalRecords / this.pageSize);
   },
 });
